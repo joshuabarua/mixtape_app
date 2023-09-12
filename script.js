fed3e3108961dda3d64f8df3d8b4bcd7ae18c6aa
Joshua Barua <joshuabarua9@hotmail.com>
2023-09-12 13:49:34 +0200

    plyr front end

--- script.js
+++ script.js
@@ -1,21 +1,59 @@
+const player = new Plyr('#plyr-player', {
+	controls: ['play', 'progress', 'volume', 'settings'],
+});
+
+window.player = player;
+
 const playlist = {
-	video1: 'https://www.youtube.com/watch?v=VIDEO_ID_1',
-	video2: 'https://www.youtube.com/watch?v=VIDEO_ID_2',
-	video3: 'https://www.youtube.com/watch?v=VIDEO_ID_3',
+	video1: {src: 'https://www.youtube.com/watch?v=V_Ydoe4Q-Gg', name: 'Radiohead - Weird Fishes'},
+	video2: {src: 'https://www.youtube.com/watch?v=qU9mHegkTc4', name: 'Arctic Monkeys - 505'},
+	video3: {src: 'https://www.youtube.com/watch?v=Iw1Fm61HBA8', name: 'Queens of the Stone Age - Go With The Flow'},
+	video4: {src: 'https://www.youtube.com/watch?v=Iw1Fm61HBA8', name: 'Current Joys - Breaking the Waves'},
+	video5: {src: 'https://www.youtube.com/watch?v=V_Ydoe4Q-Gg', name: 'Radiohead - Weird Fishes'},
+	video6: {src: 'https://www.youtube.com/watch?v=qU9mHegkTc4', name: 'Arctic Monkeys - 505'},
+	video7: {src: 'https://www.youtube.com/watch?v=Iw1Fm61HBA8', name: 'Queens of the Stone Age - Go With The Flow'},
+	video8: {src: 'https://www.youtube.com/watch?v=Iw1Fm61HBA8', name: 'Queens of the Stone Age - Go With The Flow'},
+	video9: {src: 'https://www.youtube.com/watch?v=V_Ydoe4Q-Gg', name: 'Radiohead - Weird Fishes'},
+	video10: {src: 'https://www.youtube.com/watch?v=qU9mHegkTc4', name: 'Arctic Monkeys - 505'},
+	video11: {src: 'https://www.youtube.com/watch?v=Iw1Fm61HBA8', name: 'Queens of the Stone Age - Go With The Flow'},
+	video12: {src: 'https://www.youtube.com/watch?v=Iw1Fm61HBA8', name: 'Queens of the Stone Age - Go With The Flow'},
+	video13: {src: 'https://www.youtube.com/watch?v=V_Ydoe4Q-Gg', name: 'Radiohead - Weird Fishes'},
+	video14: {src: 'https://www.youtube.com/watch?v=qU9mHegkTc4', name: 'Arctic Monkeys - 505'},
+	video15: {src: 'https://www.youtube.com/watch?v=Iw1Fm61HBA8', name: 'Queens of the Stone Age - Go With The Flow'},
+	video16: {src: 'https://www.youtube.com/watch?v=Iw1Fm61HBA8', name: 'Queens of the Stone Age - Go With The Flow'},
+	video17: {src: 'https://www.youtube.com/watch?v=V_Ydoe4Q-Gg', name: 'Radiohead - Weird Fishes'},
+	video18: {src: 'https://www.youtube.com/watch?v=qU9mHegkTc4', name: 'Arctic Monkeys - 505'},
+	video19: {src: 'https://www.youtube.com/watch?v=Iw1Fm61HBA8', name: 'Queens of the Stone Age - Go With The Flow'},
+	video20: {src: 'https://www.youtube.com/watch?v=Iw1Fm61HBA8', name: 'Queens of the Stone Age - Go With The Flow'},
+
 	// Add more videos as needed
 };
 
 const keys = Object.keys(playlist);
 let currentTrack = 0;
-let player;
 
 const playButton = document.getElementById('playButton');
 const pauseButton = document.getElementById('pauseButton');
 
+const updateTrackTitle = () => {
+	const currentKey = keys[currentTrack];
+	// Extract the track name from the key
+	document.getElementById('currentTrackTitle').textContent = `Now Playing: ${playlist[keys[currentTrack]].name}`;
+};
+
+const togglePlayPauseButtons = (isPlaying) => {
+	if (isPlaying) {
+		playButton.style.display = 'none';
+		pauseButton.style.display = 'block';
+	} else {
+		playButton.style.display = 'block';
+		pauseButton.style.display = 'none';
+	}
+};
+
 const togglePlayingState = (isPlaying) => {
 	const teethBoxes = document.querySelectorAll('.teethBox, .teethBox2');
 	const tapeRibbons = document.querySelectorAll('.tape-ribbon, .tape-ribbon-two');
-
 	if (isPlaying) {
 		teethBoxes.forEach((box) => box.classList.add('playing'));
 		tapeRibbons.forEach((ribbon) => ribbon.classList.add('play'));
@@ -25,32 +63,33 @@ const togglePlayingState = (isPlaying) => {
 	}
 };
 
-const initializePlayer = (trackIndex) => {
-	const currentKey = keys[trackIndex];
-	player = new Howl({
-		src: [playlist[currentKey]],
-		html5: true,
-		onend: () => playNextTrack(),
-	});
-};
-
 const playNextTrack = () => {
 	currentTrack = (currentTrack + 1) % keys.length;
-	player.stop();
-	initializePlayer(currentTrack);
+	player.source = {
+		type: 'video',
+		sources: [
+			{
+				src: playlist[keys[currentTrack]].src,
+				provider: 'youtube',
+			},
+		],
+	};
 	player.play();
 	togglePlayingState(true);
+	updateTrackTitle();
 };
 
 const play = () => {
-	initializePlayer(currentTrack);
 	player.play();
 	togglePlayingState(true);
+	updateTrackTitle();
+	togglePlayPauseButtons(true);
 };
 
 const pause = () => {
 	player.pause();
 	togglePlayingState(false);
+	togglePlayPauseButtons(false);
 };
 
 playButton.addEventListener('click', play);
